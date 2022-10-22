36adf1013b6d793eb15ae40efe9abe15f2451198
ouattara abraham <ouattara.abraham2019@gmail.com>
2022-10-22 12:49:56 +0200

    my application mobile of localisation

---
+++ composent/Home.js
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View, Image } from 'react-native'
+import React from 'react'
+
+ function Home() {
+  return (
+    <View >
+      <Text>sssssssssssssssssssssssssssssssssssssssssssssssssddd</Text>
+      <Image source={require("../assets/img1.png")} style={{width:200, height:200}}/>
+      
+      
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    image:{
+        
+
+    }
+})
+
+export default  Home  ; 
